775fdc5af399b8662bb4eaf4fb0ecbd2104ec419
Andrew <mr.fesenko@inbox.ru>
2022-08-26 17:01:28 +0300

    added task1

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿int Prompt(string text)
+{
+    Console.Write(text);
+    text = Console.ReadLine();
+    int result = int.Parse(text);
+    return result;
+}
+
+int [] getNeededMassive( int numbers)
+{
+    int i = 0;
+    int numberslength = Prompt("Enter a count of numbers ");
+    int [] arrays = new int [numberslength];
+    while  ( i  < numberslength)
+    {
+        Console.Write ("Enter a number ");
+        arrays[i] = int.Parse(Console.ReadLine());
+      
+    }
+    return arrays;
+}
+int CountOfPossitiveNumbers (int [] arrays)
+{
+   int count = 0;
+   int i = 0;
+   if (arrays [i] > 0)
+        {
+            count+= 1;
+            i++;
+        } 
+    return count;
+}
+
