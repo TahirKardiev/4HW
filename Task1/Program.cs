a86e229e642f9f1671eb9937562ed6ff7f0698dd
Andrew <mr.fesenko@inbox.ru>
2022-08-31 11:02:58 +0300

    added version with additions and editions

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,16 +6,17 @@
     return result;
 }
 
+int numberslength = Prompt("Enter a count of numbers ");
+
 int [] getNeededMassive( int numbers)
 {
     int i = 0;
-    int numberslength = Prompt("Enter a count of numbers ");
     int [] arrays = new int [numberslength];
     while  ( i  < numberslength)
     {
         Console.Write ("Enter a number ");
         arrays[i] = int.Parse(Console.ReadLine());
-      
+        i++;
     }
     return arrays;
 }
@@ -23,11 +24,22 @@ int CountOfPossitiveNumbers (int [] arrays)
 {
    int count = 0;
    int i = 0;
+   while  ( i  < numberslength)
+   {
    if (arrays [i] > 0)
         {
             count+= 1;
             i++;
-        } 
-    return count;
+        }   
+   else
+        {
+            count+=0;
+            i++;
+        }
+   }
+   return count;
 }
 
+int[] array = getNeededMassive(numberslength);
+int positiveNumber = CountOfPossitiveNumbers(array);
+Console.WriteLine(positiveNumber);
